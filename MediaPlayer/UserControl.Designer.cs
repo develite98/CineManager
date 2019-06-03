ed4cd5a211f60e72ab637a6ccc6aaf4ddc89b82a
tanphat12041998 <nguyentanphat.12041998@gmail.com>
2019-06-03 08:35:59 +0700

    + Handle video viewing function

--- MediaPlayer/UserControl.Designer.cs
+++ MediaPlayer/UserControl.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MediaPlayer
 {
-    partial class UserControl1
+    partial class UserControl
     {
         /// <summary>
         /// Required designer variable.
@@ -26,36 +26,36 @@
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
+
+        private AxMOVIEPLAYERLib.AxMoviePlayer axMoviePlayer;
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
-            this.axMoviePlayer1 = new AxMOVIEPLAYERLib.AxMoviePlayer();
-            ((System.ComponentModel.ISupportInitialize)(this.axMoviePlayer1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl));
+            this.axMoviePlayer = new AxMOVIEPLAYERLib.AxMoviePlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axMoviePlayer)).BeginInit();
             this.SuspendLayout();
             // 
-            // axMoviePlayer1
+            // axMoviePlayer
             // 
-            this.axMoviePlayer1.Enabled = true;
-            this.axMoviePlayer1.Location = new System.Drawing.Point(19, 16);
-            this.axMoviePlayer1.Name = "axMoviePlayer1";
-            this.axMoviePlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMoviePlayer1.OcxState")));
-            this.axMoviePlayer1.Size = new System.Drawing.Size(762, 396);
-            this.axMoviePlayer1.TabIndex = 0;
+            this.axMoviePlayer.Enabled = true;
+            this.axMoviePlayer.Location = new System.Drawing.Point(19, 16);
+            this.axMoviePlayer.Name = "axMoviePlayer";
+            this.axMoviePlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMoviePlayer.OcxState")));
+            this.axMoviePlayer.Size = new System.Drawing.Size(762, 396);
+            this.axMoviePlayer.TabIndex = 0;
             // 
-            // UserControl1
+            // UserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.axMoviePlayer1);
-            this.Name = "UserControl1";
+            this.Controls.Add(this.axMoviePlayer);
+            this.Name = "UserControl";
             this.Size = new System.Drawing.Size(800, 450);
-            ((System.ComponentModel.ISupportInitialize)(this.axMoviePlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMoviePlayer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private AxMOVIEPLAYERLib.AxMoviePlayer axMoviePlayer1;
     }
 }
--- Project/ViewModel/MainViewModel.cs
+++ Project/ViewModel/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using ProjectVideo.Models;
 
@@ -18,6 +19,10 @@ namespace ProjectVideo.ViewModel
 
         private string username = "";
         public string UserName { get; set; }
+
+        private string videoPath = "";
+        public string VideoPath { get { return videoPath; } set { videoPath = value; OnPropertyChanged(); } }
+
         public ICommand Li_LoCommand { get; set; }
         public ICommand LoadedWindowCommand { get; set; }
 
